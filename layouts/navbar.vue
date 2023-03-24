52d935e00fb2d013437076dbb31724ec896db656
yDienifer <dienifersiqueira.dev@gmail.com>
2023-03-23 22:04:35 -0300

    Neste commit, comecei revisando alguns conceitos do Vue, iniciando com inputs e data binding. Primeiramente, criei um novo componente chamado 'navbar.vue', que será responsável pela barra de navegação do projeto. Após a criação do arquivo, decidi organizá-lo na pasta 'layouts' para aprimorar a organização do projeto e facilitar a reutilização dos componentes presentes na pasta. Dentro do 'navbar.vue', iniciei a estruturação da navbar, utilizando 'data()', 'components' e 'methods', bem como aprendendo a diferença entre '<script lang=ts>' e '<script setup lang=ts>'. Até o momento, a navbar contém apenas a logo (em span), o input de pesquisa e um botão para enviar a pesquisa, além de uma lista incompleta. Também conta com um evento no input de botão, do qual, fiz o data binding e, também, adicionei um v-model à caixa de pesquisa, para assim, criar um vinculo de dados. Em resumo, este commit foi apenas uma revisão do Vue.

---
+++ layouts/navbar.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header>
+        <span>VueNexus</span>
+        <nav>
+            <input type="search" v-model="input_name" placeholder="Buscar..."> 
+            <!-- v-model = cria um vínculo bidirecional de dados, ou seja, qualquer coisa 
+            atribuída ao v-model do elemento será aplicada automaticamente no mesmo. -->
+
+            <input type="submit" v-on:click="btnParaEnviarPesquisa" value="🔎">
+            <!-- v-on = adiciona manipuladores de eventos. -->
+
+            <ul>
+                <li><a href="#">{{ input_name }}</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'BarraDeNavegação',
+    data() {
+        return { // Define dados dos componentes
+            input_name: ""
+        }
+    },
+    methods: { // Responsável por funções da aplicação
+        btnParaEnviarPesquisa(e) {
+            e.preventDefault();
+            console.log(this.input_name); /* This = Serve para referir 
+            às propriedades e métodos do objeto em que ele é chamado. */
+
+            this.name = this.input_name;
+        }
+    }
+}
+</script>
